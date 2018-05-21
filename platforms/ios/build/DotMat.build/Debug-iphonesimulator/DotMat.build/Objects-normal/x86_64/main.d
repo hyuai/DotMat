6b8ef39a54f60a93fec0f452e409f5ea15374b4c
ghatdev <issacs98@gmail.com>
2018-05-21 18:43:58 +0900

    update libraries

---
+++ platforms/ios/build/DotMat.build/Debug-iphonesimulator/DotMat.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/DotMat/DotMat-Prefix.pch \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/main.m \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/TNSDebugging.h \
+  /Users/ghatdev/Work/mobile/DotMat/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
